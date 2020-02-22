6cb4be0177482e91e483e122d0aca141836e544f
Raymond Yeh <extraymond@gmail.com>
2020-02-22 17:39:15 +0800

    split example under feature gate.

---
+++ src/example.rs
@@ -0,0 +1,56 @@
+use vega_lite_3::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn call_vega() {
+    femme::start(log::LevelFilter::Info).unwrap();
+    let doc = web_sys::window().unwrap().document().unwrap();
+    let target = doc.get_element_by_id("viz").unwrap();
+
+    if let Ok(chart) = gen_chart() {
+        let mut option = HashMap::<&str, &str>::new();
+        option.insert("renderer", "svg");
+        render_chart(&chart, target, &Some(option));
+    }
+}
+
+pub fn gen_chart() -> Result<Vegalite, Box<dyn std::error::Error>> {
+    // the chart
+    let chart = VegaliteBuilder::default()
+        .title("Choropleth of Unemployment Rate per County")
+        .data(
+            UrlDataBuilder::default()
+                .url("https://raw.githubusercontent.com/vega/vega-datasets/master/data/us-10m.json")
+                .format(
+                    DataFormatBuilder::default()
+                        .data_format_type(DataFormatType::Topojson)
+                        .feature("counties")
+                        .build()?,
+                )
+                .build()?,
+        )
+        .mark(Mark::Geoshape)
+        .transform(vec![TransformBuilder::default()
+            .lookup("id")
+            .from(LookupDataBuilder::default()
+                .data(DataBuilder::default()
+                    .url("https://raw.githubusercontent.com/vega/vega-datasets/master/data/unemployment.tsv")
+                    .build()?)
+                .key("id")
+                .fields(vec!["rate".to_string()])
+                .build()?)
+            .build()?])
+        .projection(ProjectionBuilder::default().projection_type(ProjectionType::AlbersUsa).build()?)
+        .encoding(
+            EncodingBuilder::default()
+                .color(
+                    DefWithConditionMarkPropFieldDefStringNullBuilder::default()
+                        .field("rate")
+                        .def_with_condition_mark_prop_field_def_string_null_type(StandardType::Quantitative)
+                        .build()?,
+                )
+                .build()?,
+        )
+        .build()?;
+    Ok(chart)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ use vega_lite_3::*;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::*;
 
+#[cfg(feature = "example")]
+use example;
+
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -16,6 +19,7 @@ extern "C" {
     fn vegaEmbed(target: web_sys::Element, spec: JsValue, option: JsValue) -> js_sys::Promise;
 }
 
+/// Render chart onto the web_sys::Element, with optional dict.
 pub fn render_chart(
     chart: &Vegalite,
     target: web_sys::Element,
@@ -33,62 +37,4 @@ pub fn render_chart(
     });
 
     Ok(())
-}
-
-// #[wasm_bindgen(start)]
-// pub fn main() {
-// }
-
-#[wasm_bindgen]
-pub fn call_vega() {
-    femme::start(log::LevelFilter::Info).unwrap();
-    let doc = web_sys::window().unwrap().document().unwrap();
-    let target = doc.get_element_by_id("viz").unwrap();
-
-    if let Ok(chart) = gen_chart() {
-        let mut option = HashMap::<&str, &str>::new();
-        option.insert("renderer", "svg");
-        render_chart(&chart, target, &Some(option));
-    }
-}
-
-pub fn gen_chart() -> Result<Vegalite, Box<dyn std::error::Error>> {
-    // the chart
-    let chart = VegaliteBuilder::default()
-        .title("Choropleth of Unemployment Rate per County")
-        .data(
-            UrlDataBuilder::default()
-                .url("https://raw.githubusercontent.com/vega/vega-datasets/master/data/us-10m.json")
-                .format(
-                    DataFormatBuilder::default()
-                        .data_format_type(DataFormatType::Topojson)
-                        .feature("counties")
-                        .build()?,
-                )
-                .build()?,
-        )
-        .mark(Mark::Geoshape)
-        .transform(vec![TransformBuilder::default()
-            .lookup("id")
-            .from(LookupDataBuilder::default()
-                .data(DataBuilder::default()
-                    .url("https://raw.githubusercontent.com/vega/vega-datasets/master/data/unemployment.tsv")
-                    .build()?)
-                .key("id")
-                .fields(vec!["rate".to_string()])
-                .build()?)
-            .build()?])
-        .projection(ProjectionBuilder::default().projection_type(ProjectionType::AlbersUsa).build()?)
-        .encoding(
-            EncodingBuilder::default()
-                .color(
-                    DefWithConditionMarkPropFieldDefStringNullBuilder::default()
-                        .field("rate")
-                        .def_with_condition_mark_prop_field_def_string_null_type(StandardType::Quantitative)
-                        .build()?,
-                )
-                .build()?,
-        )
-        .build()?;
-    Ok(chart)
 }
